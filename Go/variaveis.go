2051acd827e6708e0b6106456b67a40db57420e0
Caio <cvmartins1996@gmail.com>
2018-01-07 22:31:24 -0200

    Go lang

---
+++ Go/variaveis.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var alpha string = "Alpha"
+	fmt.Println(alpha)
+
+	var beta, charlie = 1, 2
+	fmt.Println(beta, charlie)
+
+	var delta = true
+	fmt.Println(delta)
+
+	var echo int
+	fmt.Println(echo)
+
+	foxtrot:= "outra maneira"
+	fmt.Println(foxtrot)
+}
